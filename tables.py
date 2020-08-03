a2dd0a14bc7170543d8e41a2a4a825b8a466458d
Raj Kaste <rajkaste@gmail.com>
2020-08-03 23:47:31 +0530

    Add files via upload

---
+++ tables.py
@@ -0,0 +1,13 @@
+# Using While Loop
+
+num = int(input('Enter the tables number:'))
+i = 1
+while (i != 11):
+    print(num,' ','x',' ',i,' ','=',' ',num*i)
+    i += 1
+
+#Using For Loop
+
+# num = int(input('Enter the tables number:'))
+# for i in range(1,11):
+#     print(num, ' ', 'x', ' ', i, ' ', '=', ' ', num*i)
